d0abfde9bf66e9be668538896e022607a16d245b
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2025-07-26 22:19:15 +0700

    Sửa logic Modal đáp ứng options mới

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+// Lấy ra các nút
 const dataModal = $$("[data-modal]");
 const btnClose = $$("#btn-close");
-const modalBackdrop = $$(".modal-backdrop");
+// const modalBackdrop = $$(".modal-backdrop");
 
+// hàm sử lý modal
+function Modal(option = {}) {
+  const { templateId, methodModal = ["button", "orvelay", "escape"] } = option;
+  const template = $(`#${templateId}`);
 
-function Modal() {
+  if (!template) {
+    console.error(`${templateId} không tồn tại`);
+    return;
+  }
+  // kiểm tra xem có các nút phù hợp hay không và nếu có thì là true / false
+  this._allowButtonClose = methodModal.includes("button");
+  this._allowOrvelayClose = methodModal.includes("orvelay");
+  this._allowEscapeClose = methodModal.includes("escape");
+
   // hàm sử lý lấy ra kích thước cảu thanh cuộn
   // kĩ thuậy Cache
   function getScrollballWidth() {
+    // do modalBackdrop cũng là 1 object lên cũng có thể chấm value để gán gia trị
     if (getScrollballWidth.value) {
       console.log("Giá trị thanh cuộn đã được lưu vào bộ nhớ");
       return getScrollballWidth.value;
@@ -31,14 +45,7 @@ function Modal() {
     console.log(`Tinhs toán kích thước của thanh cuộn: ${ScrollballWidth}`);
     return ScrollballWidth;
   }
-  this.openModal = (option = {}) => {
-    const { templateId, modalElement = true } = option;
-    const template = $(`#${templateId}`);
-
-    if (!template) {
-      console.error(`${templateId} không tồn tại`);
-      return;
-    }
+  this.open = () => {
     const content = template.content.cloneNode(true);
 
     // create element
@@ -48,56 +55,62 @@ function Modal() {
 
     const modalContainer = document.createElement("div");
     modalContainer.className = "modal-container";
+    if (this._allowButtonClose) {
+      // tạo element button
+      const modalClose = document.createElement("button");
+      modalClose.className = "modal-close";
+      modalClose.id = "btn-close";
+      // tạo element i
+      const i = document.createElement("i");
+      i.className = "fa-solid fa-xmark";
+      modalClose.appendChild(i);
+      modalContainer.append(modalClose);
 
-    const modalClose = document.createElement("button");
-    modalClose.className = "modal-close";
-    modalClose.id = "btn-close";
-
-    const i = document.createElement("i");
-    i.className = "fa-solid fa-xmark";
-    modalClose.appendChild(i);
-
+      // hàm sử lý xóa
+      modalClose.onclick = () => {
+        this.close(modalBackdrop);
+      };
+    }
+    // tạo thẻ elemnt div content
     const modalContent = document.createElement("div");
     modalContent.className = "modal-content";
 
     modalContent.append(content);
 
     // Appent content and element
-    modalContainer.append(modalClose, modalContent);
+    modalContainer.append(modalContent);
     modalBackdrop.appendChild(modalContainer);
     document.body.appendChild(modalBackdrop);
 
-    // lắng nghe sự kiện click
-
+    // khi modalBackdrop được cho vào Dom thì sau khoản thời gian thì thêm class show vào
     setTimeout(() => {
       modalBackdrop.classList.add("show");
     }, 0);
 
     document.body.classList.add("croll-hidden");
     document.body.style.paddingRight = getScrollballWidth() + "px";
-
-    modalClose.onclick = () => {
-      this.closeModal(modalBackdrop);
-    };
-
-    modalBackdrop.onclick = (e) => {
-      if (modalElement) {
+    // nếu có Orvelay thì sử lý logic
+    if (this._allowOrvelayClose) {
+      modalBackdrop.onclick = (e) => {
         if (e.target === modalBackdrop) {
-          this.closeModal(modalBackdrop);
+          this.close(modalBackdrop);
         }
-      }
-    };
-
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        this.closeModal(modalBackdrop);
-      }
-    });
+      };
+    }
+    // nếu có Escape thì sử lý logic
+    if (this._allowEscapeClose) {
+      document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+          this.close(modalBackdrop);
+        }
+      });
+    }
     return modalBackdrop;
   };
-
-  this.closeModal = (modalBackdrop) => {
+  // hàm sử lý đóng 
+  this.close = (modalBackdrop) => {
     modalBackdrop.classList.remove("show");
+    // lắng nghe khi hết sự kiện transitionend thì remove ra khỏi Dom
     modalBackdrop.ontransitionend = function () {
       modalBackdrop.remove();
     };
@@ -107,32 +120,31 @@ function Modal() {
 }
 
 const modal1 = new Modal({
-    templateId: "modal1"
+  templateId: "modal1",
 });
 
 const btnModal1 = $("#modal-1");
 const btnModal2 = $("#modal-2");
 
-
 btnModal1.onclick = function () {
-  modal1.opend();
+  modal1.open();
 
   // modal1.close();
 };
 
-
 const modal2 = new Modal({
-    templateId: "modal2",
-    // closeMethods: ['button','overlay','escape'],
-    // hiển thị footer của modal
-    footer: true,
-    cssClass: ['class1','class2','classN'],
-    onOpen: () => {
-        console.log("Mddal opened")
-    },
-    onclose: () => {
-        console.log("Modal close")
-    }
+  templateId: "modal2",
+  methodModal: ["button", "escape"],
+  // closeMethods: ['button','overlay','escape'],
+  // hiển thị footer của modal
+  footer: true,
+  cssClass: ["class1", "class2", "classN"],
+  onOpen: () => {
+    console.log("Mddal opened");
+  },
+  onclose: () => {
+    console.log("Modal close");
+  },
 });
 
 //  modal2.opend()
@@ -143,9 +155,7 @@ const modal2 = new Modal({
 // mdoal2.destroy();
 
 btnModal2.onclick = function () {
-  const modalBackdrop = modal2.opend();
-
-  // modal2.close();
+  const modalBackdrop = modal2.open();
 
   const form = modalBackdrop.querySelector("#login-form");
   if (form) {
