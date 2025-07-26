da64bd37d85ac379119cbfbf19a82ac98c1f17c4
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2025-07-26 10:44:00 +0700

    Modal from template

--- js/main.js
+++ js/main.js
@@ -5,46 +5,81 @@ const dataModal = $$("[data-modal]");
 const btnClose = $$("#btn-close");
 const modalBackdrop = $$(".modal-backdrop");
 
-let currentModal = null;
+function Modal() {
+  this.openModal = (content) => {
+    // create element
+    const modalBackdrop = document.createElement("div");
+    modalBackdrop.className = "modal-backdrop";
+    modalBackdrop.id = "modal-1";
 
-dataModal.forEach((btn) => {
-    btn.onclick = function(){
-        const modal = $(this.dataset.modal);
-        if(modal){
-            modal.classList.add("show");
-        }
-        else{
-            console.error(`${this.dataset.modal} không tồn tại`);
-        }
-        currentModal = modal;
-    }
-})
+    const modalContainer = document.createElement("div");
+    modalContainer.className = "modal-container";
 
-btnClose.forEach((btn) => {
-    btn.onclick = function(){
-        const modal = this.closest(".modal-backdrop");
-        if(modal){
-            modal.classList.remove("show");
-        }
-        currentModal = null;
-    }
-})
+    const modalClose = document.createElement("button");
+    modalClose.className = "modal-close";
+    modalClose.id = "btn-close";
 
-modalBackdrop.forEach((modal) =>{
-    modal.onclick = function(e){
-       if(e.target === this){
-        modal.classList.remove("show");
-       }
-        currentModal = null;
-    }
-})
+    const i = document.createElement("i");
+    i.className = "fa-solid fa-xmark";
+    modalClose.appendChild(i);
+
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
+
+    modalContent.innerHTML = content;
+
+    // Appent content and element
+    modalContainer.append(modalClose, modalContent);
+    modalBackdrop.appendChild(modalContainer);
+    document.body.appendChild(modalBackdrop);
+
+    // lắng nghe sự kiện click
+
+    setTimeout(() => {
+      modalBackdrop.classList.add("show");
+    }, 500);
+
+    modalClose.onclick = () => {
+      this.closeModal(modalBackdrop);
+    };
+
+    modalBackdrop.onclick = (e) => {
+      if (e.target === modalBackdrop) {
+        this.closeModal(modalBackdrop);
+      }
+    };
+
+    document.addEventListener("keydown", (e) => {
+      if ((e.key === "Escape")) {
+        this.closeModal(modalBackdrop);
+      }
+    });
+  };
+  
+  this.closeModal = (modalBackdrop) => {
+    modalBackdrop.classList.remove("show");
+    modalBackdrop.ontransitionend = function () {
+      modalBackdrop.remove();
+    };
+  };
+}
+
+const modal = new Modal();
+
+const btnModal1 = $("#modal-1");
+const btnModal2 = $("#modal-2");
+const btnModal3 = $("#modal-3");
+
+btnModal1.onclick = function(){
+    modal.openModal("<h1>Manh Thang</h1>");
+}
 
 
-document.addEventListener("keydown", (e) => {
-    if(e.key === "Escape" && currentModal){
-        currentModal.classList.remove("show");
-        currentModal = null;
-    }
-})
+btnModal2.onclick = function(){
+    modal.openModal("<h1>Manh Thang</h1>");
+}
 
 
+btnModal3.onclick = function(){
+    modal.openModal("<h1>Manh Thang</h1>");
+}
